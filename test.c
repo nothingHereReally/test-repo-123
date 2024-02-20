452f49b5540e0b13104af60e265f861dedfcf66a
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-02-20 14:00:50 +0800

    added something to test.c
    just a test work in school

--- test.c
+++ test.c
@@ -4,6 +4,7 @@ int main(int argc, char** argv){
 	printf("hello world\n");
 	printf("hello world blah 22\n");
 	printf("hello world blah 333333\n");
+	printf("hello from school here");
 
 	return 0;
 }

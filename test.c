c7fd18563def0f9e39ef283cba208403219c4e18
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-02-26 21:33:51 +0800

    to have coflict later this on branch004
    should have a conflict with main branch

--- test.c
+++ test.c
@@ -37,6 +37,7 @@ int main(int argc, char** argv){
 
 	printf("ladida ahhh testing conflict main and branch003, now see conflict\n");
 	printf("testing conflect on branch\n");
+	printf("testing conflect on branch004 on purpose\n");
 	/* above 2 lines conflict on same line( current line - 6 = 34) */
 	/* both two shall not have it */
 

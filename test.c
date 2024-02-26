923d957e4dbadd47db085c17d41fb4e18c041875
nothingHereReally <johnwilliamy53@gmail.com>
2024-02-26 14:10:02 +0800

    added sign gpg key on test.c
    fuck yeah GPG key public/private key
    
    Signed-off-by: nothingHereReally <johnwilliamy53@gmail.com>

--- test.c
+++ test.c
@@ -8,6 +8,7 @@ int main(int argc, char** argv){
 	printf("hello from gpg sign something\n");
 	printf("hello from gpg sign something\n");
 	printf("hello from gpg sign something\n");
+	printf("hello from gpg sign something 999 99 9\n");
 
 	return 0;
 }
